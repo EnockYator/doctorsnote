30539925e540bf700fac2a4cf112edd78ce391df
Enock Yator <ekyator02@gmail.com>
2024-11-19 16:34:16 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ShoppingList from './components/ShoppingList.jsx';
+import './App.css';
+
+export default function App() {
+    return(
+        <>
+                <ShoppingList />      
+        </>
+    )
+}
